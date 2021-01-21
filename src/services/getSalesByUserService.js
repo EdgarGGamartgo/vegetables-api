961d2121ce9e4e9588b3a69f375517a85a889595
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-01-21 14:18:08 -0600

    Updates

--- src/services/getSalesByUserService.js
+++ src/services/getSalesByUserService.js
@@ -1,8 +1,25 @@
 import { Usuario } from '../models/Usuario'
 import { Venta } from '../models/Venta'
+import { Op } from 'sequelize'
 
+// User-Venta One to Many
+// Venta-User Many to One
 export const GetSalesByUserService = async() => {
-    return await Usuario.findAll({
-        include: Venta
+    // const userSales = await Usuario.findAll({
+    //     include: Venta
+    // }) 
+    // {
+    //     where: {
+    //         updatedAt: 
+    //     }
+    // },
+    return await Venta.findAll({
+        where: {
+            updatedAt: {
+                [Op.lt]: new Date(),
+                [Op.gt]: new Date(new Date() - 24 * 60 * 60 * 1000)
+            }
+        },
+        include: Usuario
     }) 
 }
